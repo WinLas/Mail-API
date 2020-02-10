d96256d1cbf33b794f1cc8a92a6a96a727293ba9
Robin Eskilsson <robin.eskilsson@winlas.se>
2020-02-10 08:54:19 +0100

    Initial commit

---
+++ Controllers/MailController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mail_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mail_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        // GET: api/Mail
+        [HttpGet]
+        public IEnumerable<Mail> Get()
+        {
+            return new Mail[] { new Mail() };
+        }
+
+        // GET: api/Mail/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Mail
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Mail/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Mail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Mail_API.Models
+{
+    public class Mail
+    {
+        public string Id { get; set; }
+        public string ExternalId { get; set; }
+        public MailStatus Status { get; set; } = MailStatus.Unsent;
+        public DateTime CreatedTime { get; set; }
+        public DateTime? SentTime { get; set; }
+        public string TrackerId { get; set; }
+        public DateTime? OpenTime { get; set; }
+        public IPAddress IPAdress { get; set; }
+        public MailAddress Receiver { get; set; }
+        public MailAddress Sender { get; set; }
+        public MailAddress? ReplyTo { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
+
+namespace Mail_API
+{
+    public enum MailStatus
+    {
+        Unsent=0,Sent=1,Error=2,Opened=3,Invalid
+    }
+}
