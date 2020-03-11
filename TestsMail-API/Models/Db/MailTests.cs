303b8ca61de435bdbc03f15c75e40b97fdb75cd3
Robin Eskilsson <robin.eskilsson@winlas.se>
2020-03-11 15:59:26 +0100

    3 testfall för mail-api

---
+++ TestsMail-API/Models/Db/MailTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mail_API.Models.Db.Tests
+{
+    [TestClass()]
+    public class MailTests
+    {
+        private Mail mailtest = new Mail();
+        [TestMethod()]
+        public void NoValidReceiver()
+        {
+            mailtest.Body = "Body";
+            mailtest.Receiver = "fredrik";
+            mailtest.Sender = "Fredrik";
+        }
+
+       [TestMethod()]
+        public void NoValidBody()
+        {
+            mailtest.Body = "";
+            mailtest.Receiver = "fredrik@winlas.se";
+            mailtest.Sender = "Fredrik";
+            Assert.IsFalse(mailtest.IsValid());
+        }
+
+        [TestMethod()]
+        public void IsValidMail()
+        {
+            mailtest.Body = "Body";
+            mailtest.Receiver = "fredrik@winlas.se";
+            mailtest.Sender = "Fredrik";
+            Assert.IsFalse(mailtest.IsValid());
+        }
+    }
+}
