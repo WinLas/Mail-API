faaf5c943dafb4d50f53f76cb439465f07495d06
reskilsson <robin.eskilsson@winlas.se>
2021-05-31 13:17:45 +0200

    Uppdaterade loggrader

--- Mail-API/Models/Logging/RequestLoggingMiddleware.cs
+++ Mail-API/Models/Logging/RequestLoggingMiddleware.cs
@@ -6,40 +6,79 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.IO;
 
 namespace Mail_API.Models.Logging
 {
     public class RequestLoggingMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly ILogger _logger;
-        private readonly ILog _nLog;
+        private readonly ILog _logger;
+        private readonly RecyclableMemoryStreamManager _recyclableMemoryStreamManager;
 
         public RequestLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory, ILog nLog)
         {
             _next = next;
-            _logger = loggerFactory.CreateLogger<RequestLoggingMiddleware>();
-            _nLog = nLog;
+            _logger = nLog;
+            _recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
         }
-
         public async Task Invoke(HttpContext context)
         {
-
-            try
-            {
-                await _next(context);
-            }
-            finally
-            {
-                _nLog.Information(
-                        "Request {method} {url} {ipAddress} {body} => {statusCode}" +
-                        context.Request?.Method +
-                        context.Request?.Path.Value +
-                        context.Response?.StatusCode +
-                        context.Connection.RemoteIpAddress?.ToString()
-                );
-
-            }
+            await LogRequest(context);
+            await LogResponse(context);
         }
+
+        private static string ReadStreamInChunks(Stream stream)
+        {
+            const int readChunkBufferLength = 4096;
+            stream.Seek(0, SeekOrigin.Begin);
+            using var textWriter = new StringWriter();
+            using var reader = new StreamReader(stream);
+            var readChunk = new char[readChunkBufferLength];
+            int readChunkLength;
+            do
+            {
+                readChunkLength = reader.ReadBlock(readChunk,
+                    0,
+                    readChunkBufferLength);
+                textWriter.Write(readChunk, 0, readChunkLength);
+            } while (readChunkLength > 0);
+            return textWriter.ToString();
+        }
+
+        private async Task LogRequest(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+            await using var requestStream = _recyclableMemoryStreamManager.GetStream();
+            await context.Request.Body.CopyToAsync(requestStream);
+            _logger.Information($"Http Request Information:{Environment.NewLine}" +
+                                   $"Schema:{context.Request.Scheme} " +
+                                   $"Host: {context.Request.Host} " +
+                                   $"Path: {context.Request.Path} " +
+                                   $"IpAddress: {context.Connection.RemoteIpAddress.ToString()} " +
+                                   $"QueryString: {context.Request.QueryString.Value} " +
+                                   $"Request Body: {ReadStreamInChunks(requestStream)}");
+            context.Request.Body.Position = 0;
+        }
+
+        private async Task LogResponse(HttpContext context)
+        {
+            var originalBodyStream = context.Response.Body;
+            await using var responseBody = _recyclableMemoryStreamManager.GetStream();
+            context.Response.Body = responseBody;
+            await _next(context);
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+            var text = await new StreamReader(context.Response.Body).ReadToEndAsync();
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+            _logger.Information($"Http Response Information:{Environment.NewLine}" +
+                                   $"Schema:{context.Request.Scheme} " +
+                                   $"Host: {context.Request.Host} " +
+                                   $"Path: {context.Request.Path} " +
+                                   $"IpAddress: {context.Connection.RemoteIpAddress.ToString()} " +
+                                   $"QueryString: {context.Request.QueryString.Value} " +
+                                   $"Response Body: {text}");
+            await responseBody.CopyToAsync(originalBodyStream);
+        }
+
     }
 }
