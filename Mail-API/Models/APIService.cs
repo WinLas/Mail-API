fe2d3367c605b64085b9c89ac9aeb4e875309f75
reskilsson <robin.eskilsson@winlas.se>
2021-06-08 13:41:36 +0200

    wlo-url i appsettings

--- Mail-API/Models/APIService.cs
+++ Mail-API/Models/APIService.cs
@@ -1,20 +1,24 @@
 ﻿using RestSharp;
 using RestSharp.Authenticators;
 using System.Net;
+using Microsoft.Extensions.Configuration;
 
 namespace Mail_API.Models
 {
     public class APIService
     {
-        public APIService()
+        private IConfiguration _configuration;
+        private readonly string _wloUrl;
+        public APIService(IConfiguration configuration)
         {
-
+            _configuration = configuration;
+            _wloUrl = _configuration.GetSection("Parameters").GetSection("WLO-URL").Value;
         }
 
         public bool VerifyKey(string apiKey)
         {
-            var client = new RestClient("https://localhost");
-            RestRequest request = new RestRequest("/wloffice/api/key", Method.POST);
+            var client = new RestClient(_wloUrl);
+            RestRequest request = new RestRequest("/api/key", Method.POST);
             client.Authenticator = new HttpBasicAuthenticator(apiKey,"");
             var response = client.Execute(request);
             var content = response.Content;
